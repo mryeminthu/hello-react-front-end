a01332d7c91ba18dc8c44240fda55fd0e8831d9c
mryeminthu <dhammaarchive@hotmail.com>
2024-02-13 09:14:42 +0630

    Create action, index and reducer

---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+import { FETCH_GREETING_SUCCESS } from './actions';
+
+const initialState = {
+  greeting: '',
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_GREETING_SUCCESS:
+      return { ...state, greeting: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
